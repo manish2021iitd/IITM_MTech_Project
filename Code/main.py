e60bfc3cdf6e94909cac77ab85c1c16be316938a
Manish kumar kumawat <75904964+manish2021iitd@users.noreply.github.com>
2025-05-18 01:37:49 +0530

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,28 @@
+from crewai import LLM, Process
+from agents import scenario_agent, bdd_writer_agent, simulator_agent
+from tasks import scenario_task, bdd_writer_task, simulator_task 
 
+GEMINI_API_KEY="___________________"
+os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
+
+llm = LLM(
+    model="gemini/gemini-2.0-flash",
+    temperature=0.7,
+)
+
+
+agents = [scenario_agent,bdd_writer_agent,simulator_agent]
+
+tasks = [scenario_task,bdd_writer_task,simulator_task]
+
+# Initialize the Crew object
+crew = Crew(
+    agents=agents,
+    tasks=tasks,
+    process=Process.sequential,
+    full_output=True,
+    verbose=True
+)
+
+# Kickoff the process
+responses = crew.kickoff()
